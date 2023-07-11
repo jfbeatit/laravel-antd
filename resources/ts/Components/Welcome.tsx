2cd30c3af6203d5108d06cd4914425891dc21cb0
jfbeatit <jfbeatit2023@gmail.com>
2023-07-11 19:56:01 +0800

    fix: children is optional

--- resources/ts/Components/Welcome.tsx
+++ resources/ts/Components/Welcome.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function ({
     children = "Hello World",
 }: {
-    children: React.ReactNode;
+    children?: React.ReactNode;
 }) {
     return <div>{children}</div>;
 }
