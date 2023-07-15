d13b615e0ecb95df9ee0148993baf3a6b1a5a5ae
jfbeatit <jfbeatit2023@gmail.com>
2023-07-15 14:36:25 +0800

    chore: update index componet

--- resources/ts/Pages/Auth/Login.tsx
+++ resources/ts/Pages/Auth/Login.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "@/css/auth.less";
 
 export default function () {
     return (
--- resources/ts/Pages/Index.tsx
+++ resources/ts/Pages/Index.tsx
@@ -2,5 +2,9 @@ import React from "react";
 import Welcome from "../Components/Welcome";
 
 export default function ({ msg }: { msg: string }) {
-    return <Welcome children={<h1>{msg}</h1>} />;
+    return (
+        <Welcome>
+            <h1>{msg}</h1>
+        </Welcome>
+    );
 }
